d4b0f50500f176c75dccc3d5b845b24f38051c3c
motoshakir <motoshakir@gmail.com>
2022-07-10 16:07:53 +0530

    Updates

---
+++ precache-manifest.0f83896de2a49ce1f12ddcb0c708a196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6cb6e61269b0eb183ca",
+    "url": "/my-Textapp/static/css/main.7b6a1dcd.chunk.css"
+  },
+  {
+    "revision": "c6cb6e61269b0eb183ca",
+    "url": "/my-Textapp/static/js/main.d2cd7940.chunk.js"
+  },
+  {
+    "revision": "b4896ec837a4b0c752d1",
+    "url": "/my-Textapp/static/js/runtime~main.8d890139.js"
+  },
+  {
+    "revision": "b9fd46e79bc65e36a71f",
+    "url": "/my-Textapp/static/js/2.3a713592.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/my-Textapp/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "ca383c1acfb6478af29fd5573327f447",
+    "url": "/my-Textapp/index.html"
+  }
+];
